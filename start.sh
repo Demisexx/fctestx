cdef0a3981c8c52c6630d17ea30cc66c53f91bef
Demisexx <90869940+Demisexx@users.noreply.github.com>
2022-07-23 01:13:56 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
-rm telegram_gcloner/config.ini
-curl $CONFIG_FILE_URL >> telegram_gcloner/config.ini
+cd ./telegram_gcloner
+echo -e "[General]\npath_to_gclone = ./fclone\n\ntelegram_token = $BOT_TOKEN\nuser_ids = $USER_ID\ngroup_ids = $GRP_ID\n\ngclone_para_override = $GC_PARA_OVERRIDE">> config.ini
+chmod 777 config.ini
+cd ..
 npm install http-server -g
-http-server -p 8080 &
+http-server -p $PORT &
 python3 telegram_gcloner/telegram_gcloner.py
